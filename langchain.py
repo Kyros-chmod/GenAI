266bf5419d4090db51045e554752d2231eecf758
Kyros-chmod <kyrossingh@gmail.com>
2025-04-21 11:31:10 +0530

    Add files via upload

---
+++ langchain.py
@@ -0,0 +1,12 @@
+"""
+Why Langchain?
+Langchain is a complete ecosystem. It comprises of multiple internal components.
+1. Langchain it self has a community, where we have models, retrieval, and agent tooling. 
+2. Then we have LnagServe. Chains as RestAPIs.
+3. Then we have Langsmith, which heps in 
+    i. Debugging
+    ii. Playground
+    iii. Eval
+    iv. Annotation
+    v. Monitoring
+"""
